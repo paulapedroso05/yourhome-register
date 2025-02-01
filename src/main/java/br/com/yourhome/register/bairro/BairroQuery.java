8fd01b4b291378840b274a858a148672e1f9c6ee
paulapedroso05 <paahpedroso05@gmail.com>
2025-02-01 17:59:25 -0300

    Busca de bairro e cidade por id

--- src/main/java/br/com/yourhome/register/bairro/BairroQuery.java
+++ src/main/java/br/com/yourhome/register/bairro/BairroQuery.java
@@ -1,5 +1,7 @@
 package br.com.yourhome.register.bairro;
 
+import br.com.yourhome.register.configuracao.MessageSourceHelper;
+import br.com.yourhome.register.configuracao.exception.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,8 +15,14 @@ public class BairroQuery {
 
     private final BairroRepository bairroRepository;
 
+    private final MessageSourceHelper messageSourceHelper;
+
     public Page<Bairro> findAll(String codigoIbge, Pageable pageable) {
         return bairroRepository.findBairrosByCodigoIbge(codigoIbge, pageable);
+    }
+
+    public Bairro findById(Integer id) {
+        return bairroRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(messageSourceHelper.getMessage("bairro.nao.encontrado", id)));
     }
 
 }
--- src/main/java/br/com/yourhome/register/cidade/CidadeQuery.java
+++ src/main/java/br/com/yourhome/register/cidade/CidadeQuery.java
@@ -1,5 +1,7 @@
 package br.com.yourhome.register.cidade;
 
+import br.com.yourhome.register.configuracao.MessageSourceHelper;
+import br.com.yourhome.register.configuracao.exception.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,12 +15,19 @@ public class CidadeQuery {
 
     private final CidadeRepository cidadeRepository;
 
+    private final MessageSourceHelper messageSourceHelper;
+
     public Page<Cidade> findAll(Pageable pageable) {
         return cidadeRepository.findAll(pageable);
     }
 
     public Cidade findOne(String codigoIbge) {
         return cidadeRepository.findCidadeByCodigoIbge(codigoIbge);
+    }
+
+    public Cidade findById(Integer id) {
+        return cidadeRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException(messageSourceHelper.getMessage("cidade.nao.encontrada", id)));
     }
 
 }
