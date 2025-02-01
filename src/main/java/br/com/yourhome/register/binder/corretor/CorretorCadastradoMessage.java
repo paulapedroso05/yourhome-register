8825e955b2ffa3940262b522fa6ac2094929bcfb
paulapedroso05 <paahpedroso05@gmail.com>
2025-02-01 18:00:02 -0300

    Simplificando mensagem de corretor cadastrado

--- src/main/java/br/com/yourhome/register/binder/corretor/CorretorCadastradoMessage.java
+++ src/main/java/br/com/yourhome/register/binder/corretor/CorretorCadastradoMessage.java
@@ -1,6 +1,8 @@
 package br.com.yourhome.register.binder.corretor;
 
 import br.com.yourhome.register.corretor.Corretor;
+import br.com.yourhome.register.corretor.CorretorConstrutora;
+import br.com.yourhome.register.corretor.CorretorTipoImovel;
 import lombok.*;
 
 import java.io.Serializable;
@@ -22,24 +24,20 @@ public class CorretorCadastradoMessage implements Serializable {
 
     private String bio;
 
-    private List<CorretorTipoImovelMessage> tiposImoveis;
+    private List<String> tiposImoveis;
 
-    private List<CorretorConstrutoraMessage> construtoras;
+    private List<String> construtoras;
 
-    private List<CorretorCidadeBairroMessage> cidadesBairros;
+    private List<String> cidades;
 
-    private List<CorretorCidadeMessage> cidades;
+    private List<String> bairros;
 
-    public static CorretorCadastradoMessage of(Corretor corretor) {
+    public static CorretorCadastradoMessage of(Corretor corretor, List<String> cidades, List<String> bairros) {
         return new CorretorCadastradoMessage(corretor.getCreci(), corretor.getUsuario().getNome(),
                 corretor.getUsuario().getEmail(), corretor.getTelefone(), corretor.getBio(),
-                corretor.getTiposImoveis().stream().map(tipoImovel ->
-                        CorretorTipoImovelMessage.of(tipoImovel.getIdCorretorTipoImovel(), tipoImovel.getNome())).toList(),
-                corretor.getConstrutoras().stream().map(construtora ->
-                        CorretorConstrutoraMessage.of(construtora.getIdCorretorConstrutora(), construtora.getNome())).toList(),
-                corretor.getCidadesBairros().stream().map(cidadeBairro ->
-                        CorretorCidadeBairroMessage.of(cidadeBairro.getIdCorretorCidadeBairro(), cidadeBairro.getCidadeFk(), cidadeBairro.getBairroFk())).toList(),
-                corretor.getCidades().stream().map(cidade -> CorretorCidadeMessage.of(cidade.getIdCorretorCidade(), cidade.getCidadeFk())).toList());
+                corretor.getTiposImoveis().stream().map(CorretorTipoImovel::getNome).toList(),
+                corretor.getConstrutoras().stream().map(CorretorConstrutora::getNome).toList(),
+                cidades, bairros);
     }
 
 }
--- src/main/java/br/com/yourhome/register/corretor/CorretorCommand.java
+++ src/main/java/br/com/yourhome/register/corretor/CorretorCommand.java
@@ -1,7 +1,9 @@
 package br.com.yourhome.register.corretor;
 
+import br.com.yourhome.register.bairro.BairroQuery;
 import br.com.yourhome.register.binder.corretor.CorretorCadastradoMessage;
 import br.com.yourhome.register.binder.corretor.CorretorProducer;
+import br.com.yourhome.register.cidade.CidadeQuery;
 import br.com.yourhome.register.configuracao.MessageSourceHelper;
 import br.com.yourhome.register.configuracao.exception.ConflictException;
 import br.com.yourhome.register.configuracao.exception.ResourceNotFoundException;
@@ -11,12 +13,19 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static br.com.yourhome.register.configuracao.ObjectHelper.copyNonNullProperties;
 
 @Service
 @AllArgsConstructor
 @Transactional(rollbackFor = Exception.class)
 public class CorretorCommand {
+
+    private final CidadeQuery cidadeQuery;
+
+    private final BairroQuery bairroQuery;
 
     private final UsuarioCommand usuarioCommand;
 
@@ -32,7 +41,7 @@ public class CorretorCommand {
         });
         Usuario usuario = usuarioCommand.salvar(corretor.getUsuario());
         Corretor novoCorretor = corretorRepository.save(corretor.withUsuario(usuario));
-        corretorProducer.output(CorretorCadastradoMessage.of(novoCorretor));
+        corretorProducer.output(CorretorCadastradoMessage.of(novoCorretor, extrairCidades(novoCorretor), extrairBairros(novoCorretor)));
         return novoCorretor;
     }
 
@@ -40,6 +49,20 @@ public class CorretorCommand {
         Corretor corretorDb = findOne(documento);
         copyNonNullProperties(corretor, corretorDb);
         return corretorRepository.save(corretor);
+    }
+
+    private List<String> extrairCidades(Corretor corretor) {
+        List<String> cidadesNomes = new ArrayList<>();
+        corretor.getCidades().stream().forEach(cidade ->
+                cidadesNomes.add(cidadeQuery.findById(cidade.getCidadeFk()).getNome()));
+        return cidadesNomes;
+    }
+
+    private List<String> extrairBairros(Corretor corretor) {
+        List<String> bairrosNomes = new ArrayList<>();
+        corretor.getCidadesBairros().stream().forEach(bairro ->
+                bairrosNomes.add(bairroQuery.findById(bairro.getBairroFk()).getNome()));
+        return bairrosNomes;
     }
 
     private Corretor findOne(String documento) {
